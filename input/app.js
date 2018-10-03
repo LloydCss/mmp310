b14a6cd0c2e99c2715aea156bd4f680218ecf9ae
LloydCss <lloyd.rickman@f1006-10-34127.bmcc.cuny.edu>
2018-10-03 18:55:11 -0400

    loops

---
+++ input/app.js
@@ -0,0 +1,16 @@
+var elem = document.createElement('h1');  
+elem.textContent = "hello world";
+console.log(elem);
+document.body.appendChild(elem);
+
+const submitButton = document.getElementById('submit');
+const nameInput = document.getElementById('name');
+const bioInput = document.getElementById('bio');
+const boroughInput = document.getElementById('borough');
+
+
+submitButton.onclick = function(event) {
+
+
+
+};
---
+++ loops/loops.js
@@ -0,0 +1,9 @@
+const container =
+      document.getElementById("container");
+const images = [
+    "images/dog.jpg",
+    "images/cat.jpg",
+    "images/bird.jpg"
+    
+
+];
